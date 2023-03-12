948ec5b21bf29ca871e8fec9d6b5b2ea75e88ad2
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-03-12 10:15:49 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,45 +1,37 @@
-import os
-from os import getenv, environ
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+import sys
+from os import environ
 from dotenv import load_dotenv
-
-
 
 load_dotenv()
 
+
 class Var(object):
     MULTI_CLIENT = True
-    API_ID = int(getenv('API_ID', '15823382'))
-    API_HASH = str(getenv('API_HASH', '016d5e115a06ddfb6121823d72ae4d8c'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5606869491:AAHJbj5U_fEbYmODiezZsqWP5lIWSJalQ6Y'))
-    MULTI_TOKEN1 = str(getenv('MULTI_TOKEN1', '5499850853:AAF1saxsT9Q46H7zGs90asuTpAqND5qDgnI'))
-    MULTI_TOKEN2 = str(getenv('MULTI_TOKEN2', '6213184839:AAGDu5TrR6ZiUPB9n82e6z_Anb_b6gMr_mA'))
-    name = str(getenv('name', 'filetolinkbot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-100175430918'))
-    PORT = int(getenv('PORT', 8000))
-    BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '143.110.250.241'))
+    API_ID = int(environ.get("API_ID", "15823382"))
+    API_HASH = str(environ.get("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c"))
+    MULTI_TOKEN1 = str(environ.get("MULTI_TOKEN1", "5499850853:AAF1saxsT9Q46H7zGs90asuTpAqND5qDgnI"))
+    MULTI_TOKEN2 = str(environ.get("MULTI_TOKEN2", "6213184839:AAGDu5TrR6ZiUPB9n82e6z_Anb_b6gMr_mA"))
+    SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
+    WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    BIN_CHANNEL = int(
+        environ.get("BIN_CHANNEL", "-1001754309185")
+    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    PORT = int(environ.get("PORT", 8080))
+    BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "143.110.250.241"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5104293442").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Ipapcorn_helper'))
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
-    
-    else:
-        ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '143.110.250.241:8000')) if not ON_HEROKU or getenv('FQDN', '143.110.250.241:8000') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
-    else:
-        URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://W9Uyj61afEXSw601:W9Uyj61afEXSw601@cluster0.zsxso.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
-    KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "0").lower()) in  ("1", "true", "t", "yes", "y")
-    DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
-    USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "0").lower()) in ("1", "true", "t", "yes", "y")
+    HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
+    NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
+    HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
+    if not 5 < HASH_LENGTH < 64:
+        sys.exit("Hash length should be greater than 5 and less than 64")
+    FQDN = str(environ.get("FQDN", "143.110.250.241:8080"))
+    URL = "http{}://{}{}/".format(
+            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+        )
+    KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "true").lower()) in  ("1", "true", "t", "yes", "y")
+    DEBUG = str(environ.get("DEBUG", "t").lower()) in ("1", "true", "t", "yes", "y")
+    USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "t").lower()) in ("1", "true", "t", "yes", "y")
     ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "") or "").split(",") if x.strip("@ ")]
